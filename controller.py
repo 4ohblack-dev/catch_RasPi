a1fef6cf26fc8484f4c4d25027929958fdb8dcc2
4oh.black-dev <4oh.black@gmail.com>
2026-06-19 16:13:49 +0900

    controller test success

--- controller.py
+++ controller.py
@@ -2,27 +2,33 @@ import pygame
 import time
 
 pygame.init()
-
-screen = pygame.display.set_mode((300, 200))
-pygame.display.set_caption("Controller Test")
-
 pygame.joystick.init()
 
-print("Joystick count:", pygame.joystick.get_count())
+count = pygame.joystick.get_count()
+
+if count == 0:
+    print("Controller not found")
+    exit()
 
 joy = pygame.joystick.Joystick(0)
 joy.init()
 
-print("Name:", joy.get_name())
+print("Name:",joy.get_name())
+print("Buttons:",joy.get_numbuttons())
+print("Axes:",joy.get_numaxes())
 
 while True:
     for event in pygame.event.get():
-        pass
+        print(event)
 
-    print(
-        "axis0 =", joy.get_axis(0),
-        "axis1 =", joy.get_axis(1),
-        "button0 =", joy.get_button(0)
-    )
+    axes = [joy.get_axis(i) for i in range(joy.get_numaxes())]
+    print(axes)
 
     time.sleep(0.1)
+
+#左     横：axes 0、縦：axes 1、ボタン：axes 2、L：button 4
+#右     横：axes 3、縦：axes 4、ボタン：axes 5、R：button 5
+#右十字 下から反時計回りに　button 0~3
+#左十字 返り値が(,)で、正の方向が１、負の方向が‐1
+#share : button 8
+# options : button 9
